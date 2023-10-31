2df1ec020481719a18ce422a4b2adfaa21fe91b0
thecatblake <k222ryousuke@gmail.com>
2023-10-31 12:24:31 +0900

    update sphere_image.c

--- sphere_image.c
+++ sphere_image.c
@@ -9,70 +9,88 @@
 #include "canvas.h"
 #include "light.h"
 #include "world.h"
+#include "camera.h"
 
 int main() {
-    int width = 2500;
-    int height = 2500;
+    object_t floor;
+    object_init(&floor);
+    floor.type_name = SPHERE;
+    object_scale(&floor, vector(10, 0.001, 10));
+    floor.material.color = vector(1, 0.9, 0.9);
+    floor.material.specular = 0;
+
+    object_t left_wall;
+    object_init(&left_wall);
+    left_wall.type_name = SPHERE;
+    object_scale(&left_wall, vector(10, 0.01, 10));
+    object_rotate_x(&left_wall, M_PI / 2);
+    object_rotate_y(&left_wall, -M_PI / 4);
+    object_translate(&left_wall, vector(0, 0, 5));
+    left_wall.material = floor.material;
+
+    object_t right_wall;
+    object_init(&right_wall);
+    right_wall.type_name = SPHERE;
+    object_scale(&right_wall, vector(10, 0.01, 10));
+    object_rotate_x(&right_wall, M_PI / 2);
+    object_rotate_y(&right_wall, M_PI / 4);
+    object_translate(&right_wall, vector(0, 0, 5));
+    right_wall.material = floor.material;
+
+    object_t middle;
+    object_init(&middle);
+    middle.type_name = SPHERE;
+    object_translate(&middle, vector(-0.5, 1, 0.5));
+    middle.material.color = vector(1, 0, 0);
+    middle.material.diffuse = 0.7;
+    middle.material.specular = 0.3;
+
+    object_t right;
+    object_init(&right);
+    right.type_name = SPHERE;
+    object_translate(&right, vector(1.5, 1, -0.5));
+    object_scale(&right, vector(0.5, 0.5, 0.5));
+    right.material.color = vector(0, 1, 0);
+    right.material.diffuse = 0.7;
+    right.material.specular = 0.3;
+
+    object_t left;
+    object_init(&left);
+    left.type_name = SPHERE;
+    object_translate(&left, vector(-4.5, 1, -0.75));
+    object_scale(&left, vector(0.33, 0.33, 0.33));
+    left.material.color = vector(0, 0, 1);
+    left.material.diffuse = 0.7;
+    left.material.specular = 0.3;
+
+    world_t world;
+    world_init(&world);
+
+    point_light_t light = {point(-10, 10, -10), vector(1, 1, 1)};
+    world.light = &light;
+    world_add_object(&world, &floor);
+    world_add_object(&world, &left_wall);
+    world_add_object(&world, &right_wall);
+    world_add_object(&world, &middle);
+    world_add_object(&world, &left);
+    world_add_object(&world, &right);
+
+    int width = 2000;
+    int height = 2000;
+
+    camera_t camera;
+    camera_init(&camera, width, height, M_PI / 3);
+    camera_transform(&camera, point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0));
+
     int n_pixels = width * height;
 
     tuple_t* pixels = malloc(sizeof(tuple_t) * n_pixels);
 
-    object_t s1;
-    object_init(&s1);
-    s1.type_name = SPHERE;
-    object_scale(&s1, vector(300, 300, 300));
-    object_translate(&s1, vector(-500, 0, 0));
-    material_t material;
-    material_init(&material);
-    material.color = vector(1, 0.2, 1);
-    s1.material = material;
-
-    object_t s2;
-    object_init(&s2);
-    s2.type_name = SPHERE;
-    object_scale(&s2, vector(300, 300, 300));
-    object_translate(&s2, vector(100, 0, 0));
-    s2.material = material;
-    s2.material.color = vector(0.2, 1, 1);
-
-    object_t s3;
-    object_init(&s3);
-    s3.type_name = SPHERE;
-    object_scale(&s3, vector(300, 300, 300));
-    object_translate(&s3, vector(800, 0, 0));
-    s3.material = material;
-    s3.material.color = vector(1, 1, 0.2);
-
-    point_light_t light = {point(-1000, 1000, -1000), vector(1, 1, 1)};
-
-    world_t world;
-    world_init(&world);
-    world_add_object(&world, &s1);
-    world_add_object(&world, &s2);
-    world_add_object(&world, &s3);
-    world.light = &light;
-
-    tuple_t origin = point(0, 0, -4000);
-
-    tuple_t left_top = point(-width / 2.0, height / 2.0, 0.0);
-
-    canvas_fill(width, height, pixels, point(0, 0, 0));
-
-    // calculate pixels from left top to right bottom
-    for (int i=0; i < n_pixels; i++) {
-        int y = (int)left_top.y - i / width;
-        int x = (int)left_top.x + i % width;
-
-        ray_t ray;
-
-        ray.origin = origin;
-        ray.direction = tuple_norm(vector(x - ray.origin.x, y - ray.origin.y, -ray.origin.z));
-
-        tuple_t color = color_at(&world, &ray);
-
-        pixels[i] = color;
-    }
+    camera_render(&camera, &world, pixels);
 
     FILE* fp = fopen("sphere_image.ppm", "w");
     canvas_write_ppm(width, height, pixels, fp);
+
+    fclose(fp);
+    free(pixels);
 }
